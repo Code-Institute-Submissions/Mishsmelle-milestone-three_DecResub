d5916bced8a39b66cb00020d64934aa2a949e333
Mishsmelle <michelle.oconnor@shopify.com>
2021-08-01 10:42:55 +0000

    added upload review html page

--- app.py
+++ app.py
@@ -95,6 +95,10 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+@app.route("/upload_review")
+def upload_review():
+    return render_template("upload_review.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
